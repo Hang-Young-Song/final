0e8357dc404440768aa867367fb4a9347297ecbc
jssoleey2 <hjsol110493@gmail.com>
2024-06-10 16:54:34 +0900

    Update NLP.py

--- NLP.py
+++ NLP.py
@@ -104,8 +104,7 @@ with cols[2]:
 # Footer
 st.markdown("""
     <div style='text-align: center; margin-top: 50px; color: black;'>
-    @import url('https://fonts.googleapis.com/css2?family=Nanum+Pen+Script');
-        <h4 style = "font-family: 'Nanum Pen Scrpt', cursive;">CuPT - 당신의 사랑을 응원합니다!</h4>
+        <h4 style = "font-family: 'Nanum Pen Script', cursive;">CuPT - 당신의 사랑을 응원합니다!</h4>
         <p>© 2024 CuPT. All rights reserved.</p>
     </div>
 """, unsafe_allow_html=True)
